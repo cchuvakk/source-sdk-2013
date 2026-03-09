2d0797a2abe928246e003e4dbcba90be95b3d464
cchuvakk <d.badalov@rambler.ru>
2026-03-10 01:01:15 +0300

    okay grok write me an essay on what changes i've made

--- sp/src/game/client/AppartmentPowerDisablePanel.cpp
+++ sp/src/game/client/AppartmentPowerDisablePanel.cpp
@@ -2,7 +2,6 @@
 #include "AppartmentPowerDisablePanel.h"
 #include "vgui/IVGui.h"
 
-#include <sstream>
 
 AppartmentPowerDisablePanel::AppartmentPowerDisablePanel(vgui::Panel* pParent, const char* pMetaClassName) : CVGuiScreenPanel(pParent, pMetaClassName)
 {
@@ -32,21 +31,55 @@ bool AppartmentPowerDisablePanel::Init(KeyValues* pKeyValues, VGuiScreenInitData
 		char name[32];
 		sprintf(name, "Apartment%02d", i + 1);
 
-		m_pBtnApartment[i] = dynamic_cast<vgui::ToggleButton*>(FindChildByName(name));
-		m_pBtnApartment[i]->SetDepressedSound("ambient/machines/thumper_startup1.wav");
-		m_pBtnApartment[i]->SetReleasedSound("ambient/machines/thumper_shutdown1.wav");
+		m_pBtnApartment[i] = dynamic_cast<vgui::Button*>(FindChildByName(name));
+
+		if (m_pBtnApartment[i])
+		{
+			m_pBtnApartment[i]->SetPaintBackgroundEnabled(true);
+			//dunno if it's necessary
+			m_pBtnApartment[i]->SetBgColor(Color(170, 170, 170, 255));
+			m_pBtnApartment[i]->SetFgColor(Color(0, 0, 0, 255));
+			// ^^^ this one
+			m_pBtnApartment[i]->SetDepressedColor(Color(41, 49, 51, 255), Color(255, 255, 255, 255));
+			m_pBtnApartment[i]->SetDepressedSound("buttons/button14.wav");
+		}
 	}
 
 	return true;
 }
 
+
 void AppartmentPowerDisablePanel::OnCommand(const char* command)
 {
+	CVGuiScreenPanel::OnCommand(command);
+
+	for (int i = 0; i < 9; i++) 
+	{
+		char name[32];
+		sprintf(name, "apartment%02d", i + 1);
+
+		if (!Q_stricmp(command, name))
+		{
+			if (m_pBtnApartment[i])
+			{
+				m_pBtnApartment[i]->SetEnabled(false);
+			}
+		}
+	}
+}
+
+
+void AppartmentPowerDisablePanel::PerformLayout()
+{
+	CVGuiScreenPanel::PerformLayout();
+
 	for (int i = 0; i < 9; i++)
 	{
-		if (m_pBtnApartment[i]->IsSelected())
+		if (m_pBtnApartment[i]) 
 		{
-
+			m_pBtnApartment[i]->SetDefaultColor(Color(0, 0, 0, 255), Color(170, 170, 170, 255));
+			m_pBtnApartment[i]->SetDisabledFgColor1(Color(50, 50, 50, 255));
+			m_pBtnApartment[i]->SetDisabledFgColor2(Color(0, 0, 0, 255));
 		}
 	}
 }
--- sp/src/game/client/AppartmentPowerDisablePanel.h
+++ sp/src/game/client/AppartmentPowerDisablePanel.h
@@ -3,7 +3,7 @@
 #include <cbase.h>
 #include "c_vguiscreen.h"
 #include "vgui_controls/Label.h"
-#include "vgui_controls/ToggleButton.h"
+#include "vgui_controls/Button.h"
 
 class AppartmentPowerDisablePanel : public CVGuiScreenPanel
 {
@@ -12,12 +12,13 @@ private:
 
 	vgui::Label* m_pLblWelcome;
 
-	vgui::ToggleButton* m_pBtnApartment[9];
+	vgui::Button* m_pBtnApartment[9];
 
 public:
 	AppartmentPowerDisablePanel(vgui::Panel* pParent, const char* pMetaClassName);
 	virtual ~AppartmentPowerDisablePanel();
 
 	virtual void OnCommand(const char* command);
+	virtual void PerformLayout();
 	virtual bool Init(KeyValues* pKeyValues, VGuiScreenInitData_t* pInitData);
 };
